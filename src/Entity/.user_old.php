222232038eb61f445cfaa4b1aafdcf3a858e537a
Theo Saez <theo.saez@hotmail.fr>
2021-12-02 13:42:32 +0100

    modify user entity for SQL 5.6

--- src/Entity/.user_old.php
+++ src/Entity/.user_old.php
@@ -24,9 +24,9 @@ class User implements UserInterface
     private $email;
 
     /**
-     * @ORM\Column(type="string", length=180)
+     * @ORM\Column(type="json")
      */
-    private $roles = "[]";
+    private $roles = [];
 
     /**
      * @var string The hashed password
@@ -68,12 +68,12 @@ class User implements UserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles = 'ROLE_USER';
+        $roles[] = 'ROLE_USER';
 
-        return array_unique(array($roles));;
+        return array_unique($roles);
     }
 
-    public function setRoles(string $roles): self
+    public function setRoles(array $roles): self
     {
         $this->roles = $roles;
 
